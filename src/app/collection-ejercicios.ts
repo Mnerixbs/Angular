77ccc07b46dd19d0286d5c3423644d9ea5139d53
mario <xbsneri@gmail.com>
2020-05-10 21:47:50 -0500

    Se añadio una coleccion a ejercicios y se mostro en la vista

---
+++ src/app/collection-ejercicios.ts
@@ -0,0 +1,11 @@
+import { Ejercicio } from "./Ejercicio";
+
+export const EJERCICIOS: Ejercicio[]=[
+  {id:1, name:"flexiones"},
+  {id:2, name:"Sentadillas"},
+  {id:3, name:"Curl Biceps"},
+  {id:4, name:"Dominada"},
+  {id:5, name:"Plancha"},
+  {id:6, name:"Press mancuerna hombro"}
+];
+
